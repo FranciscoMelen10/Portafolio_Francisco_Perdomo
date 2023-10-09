0af75b7f65c5ae34f3e7f88112b4af086e725197
FranciscoMelen10 <francisco.melendez14454@est.uca.edu.ni>
2023-10-09 00:18:17 -0600

    Initial commit

---
+++ Proyecto/src/JS/swiper_pagination.js
@@ -0,0 +1,41 @@
+const swiperEl = document.querySelector('swiper-container')
+Object.assign(swiperEl, {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
+
+swiperEl.initialize();
+
+
+//Se encontro un bug en donde el swiper se coloca encima del header y este es la solución:
+const abrir = document.querySelector("#hambuerguesa");
+const cerrar = document.querySelector("#close");
+
+abrir.addEventListener("click", () => {
+  swiperEl.classList.add("ocultar");
+});
+
+cerrar.addEventListener("click", () => {
+  swiperEl.classList.remove("ocultar");
+});
